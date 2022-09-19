0c1c325ca94c09ac36e5f15f1f11fdad9df90c03
shinkai-tester <alexandra.klimantowa@gmail.com>
2022-09-19 06:31:11 +0300

    Ex5 is done - Get Json HW

---
+++ src/test/java/GetJsonHwTest.java
@@ -0,0 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+public class GetJsonHwTest {
+
+    @Test
+    public void testGetJsonHw(){
+
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        String secondMessage = response.getString("messages[1].message");
+        System.out.print(secondMessage);
+
+    }
+}
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,13 +2,38 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/hello")
+    public void testRestAssured() {
+        Map<String, String> data = new HashMap<>();
+        data.put("login", "secret_login");
+        data.put("password", "secret_pass");
+
+        Response responseForGet = RestAssured
+                .given()
+                .body(data)
+                .when()
+                .post("https://playground.learnqa.ru/api/get_auth_cookie")
                 .andReturn();
-        response.prettyPrint();
+
+        String responseCookie = responseForGet.getCookie("auth_cookie");
+
+        Map<String, String> cookies = new HashMap<>();
+        if (responseCookie != null) {
+            cookies.put("auth_cookie", responseCookie);
+        }
+
+        Response responseForCheck = RestAssured
+                .given()
+                .cookies(cookies)
+                .when()
+                .get("https://playground.learnqa.ru/api/check_auth_cookie")
+                .andReturn();
+
+        responseForCheck.print();
     }
 }

e97075280da835a7975626a0ae7f600d5ae64f6c
shinkai-tester <alexandra.klimantowa@gmail.com>
2022-12-30 22:32:30 +0100

    added negative parameterized tests in UserAuthTest

--- src/test/java/open_user_api/UserAuthTest.java
+++ src/test/java/open_user_api/UserAuthTest.java
@@ -4,7 +4,10 @@ import io.restassured.RestAssured;
 import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -46,5 +49,36 @@ public class UserAuthTest {
         int userIdOnCheck = responseCheckAuth.getInt("user_id");
         assertTrue(userIdOnCheck > 0, "Unexpected user id " + userIdOnCheck);
         assertEquals(userIdOnAuth, userIdOnCheck, "User id from auth request is not equal to user id from check request");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"cookie", "headers"})
+    public void testNegativeAuthUser(String condition) {
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        Map<String, String> cookies = responseGetAuth.getCookies();
+        Headers headers = responseGetAuth.getHeaders();
+
+        RequestSpecification spec = RestAssured.given();
+        spec.baseUri("https://playground.learnqa.ru/api/user/auth");
+
+        if (condition.equals("cookie")) {
+            spec.cookie("auth_sid", cookies.get("auth_sid"));
+        } else if (condition.equals("headers")) {
+            spec.header("x-csrf-token", headers.get("x-csrf-token"));
+        } else {
+            throw new IllegalArgumentException("Condition value is known: " + condition);
+        }
+
+        JsonPath responseForCheck = spec.get().jsonPath();
+        assertEquals(0, responseForCheck.getInt("user_id"), "user_id should be 0 for unauthorized request");
     }
 }

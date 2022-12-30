ddf0ee3dbbf1eef9bbc9a6f6ddda7f3a380d2cd9
shinkai-tester <alexandra.klimantowa@gmail.com>
2022-12-30 23:10:39 +0100

    added @BeforeEach in UserAuthTest

--- src/test/java/open_user_api/UserAuthTest.java
+++ src/test/java/open_user_api/UserAuthTest.java
@@ -1,10 +1,10 @@
 package open_user_api;
 
 import io.restassured.RestAssured;
-import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -17,8 +17,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserAuthTest {
 
-    @Test
-    public void testAuthUser() {
+    String cookie;
+    String header;
+    int userIdOnAuth;
+
+    @BeforeEach
+    public void loginUser() {
         Map<String, String> authData = new HashMap<>();
         authData.put("email", "vinkotov@example.com");
         authData.put("password", "1234");
@@ -29,20 +33,17 @@ public class UserAuthTest {
                 .post("https://playground.learnqa.ru/api/user/login")
                 .andReturn();
 
-        Map<String, String> cookies = responseGetAuth.getCookies();
-        Headers headers = responseGetAuth.getHeaders();
-        int userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
+        this.cookie = responseGetAuth.getCookie("auth_sid");
+        this.header = responseGetAuth.getHeader("x-csrf-token");
+        this.userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
+    }
 
-        assertEquals(200, responseGetAuth.statusCode(), "Unexpected status code");
-        assertTrue(cookies.containsKey("auth_sid"), "Response doesn't have 'auth_sid' cookie");
-        assertTrue(headers.hasHeaderWithName("x-csrf-token"), "Response doesn't have 'x-csrf-token' header");
-        assertTrue(userIdOnAuth > 0, "User id should be greater than 0");
-
-
+    @Test
+    public void testAuthUser() {
         JsonPath responseCheckAuth = RestAssured
                 .given()
-                .header("x-csrf-token", responseGetAuth.getHeader("x-csrf-token"))
-                .cookie("auth_sid", responseGetAuth.getCookie("auth_sid"))
+                .header("x-csrf-token", header)
+                .cookie("auth_sid", cookie)
                 .get("https://playground.learnqa.ru/api/user/auth")
                 .jsonPath();
 
@@ -54,26 +55,13 @@ public class UserAuthTest {
     @ParameterizedTest
     @ValueSource(strings = {"cookie", "headers"})
     public void testNegativeAuthUser(String condition) {
-        Map<String, String> authData = new HashMap<>();
-        authData.put("email", "vinkotov@example.com");
-        authData.put("password", "1234");
-
-        Response responseGetAuth = RestAssured
-                .given()
-                .body(authData)
-                .post("https://playground.learnqa.ru/api/user/login")
-                .andReturn();
-
-        Map<String, String> cookies = responseGetAuth.getCookies();
-        Headers headers = responseGetAuth.getHeaders();
-
         RequestSpecification spec = RestAssured.given();
         spec.baseUri("https://playground.learnqa.ru/api/user/auth");
 
         if (condition.equals("cookie")) {
-            spec.cookie("auth_sid", cookies.get("auth_sid"));
+            spec.cookie("auth_sid", this.cookie);
         } else if (condition.equals("headers")) {
-            spec.header("x-csrf-token", headers.get("x-csrf-token"));
+            spec.header("x-csrf-token", this.header);
         } else {
             throw new IllegalArgumentException("Condition value is known: " + condition);
         }
